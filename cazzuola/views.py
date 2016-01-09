1dd521e611dda3859ad714435880422e906edbb8
Mirko Mucaria <skogsfrae@gmail.com>
2016-01-09 21:03:03 +0100

    Nuova interfaccia di base

--- cazzuola/views.py
+++ cazzuola/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 
+
 def index(request):
-	
-	content = {
-		"titolo_pagina": "Cazzuola prova",
-		"titolo_testo": "Titolo di prova",
-		"testo": "testo di prova"
-	}
-	
-	return render(request, 'base_template.html', content)
+
+    return render(request, 'base.html')
+
